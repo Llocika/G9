fa2b626911f1180318e6a50c77db4bf711b238e6
Llocika <72881765+Llocika@users.noreply.github.com>
2021-03-23 10:03:14 +0200

    Update teksts.cpp

--- teksts.cpp
+++ teksts.cpp
@@ -3,20 +3,20 @@
 #include <iostream>
 using namespace std;
 
-int main ()
+int main()
 {
     fstream fin;
     char c;
     int filesize = 0;
-    fin.open ("lab19in.txt", ios::in);
-    fin.get (c);
+    fin.open("lab19in.txt", ios::in);
+    fin.get(c);
     while (fin)
     {
         filesize++;
         cout << c;
-        fin.get (c);
+        fin.get(c);
     };
-    fin.close ();
+    fin.close();
     cout << endl << filesize << endl;
     return 0;
 }
@@ -56,30 +56,30 @@ int main()
 /*1[6]. Programma addLineNumbering, kas pārraksta faila saturu otrā failā, katrai rindai priekšā pieliekot tās kārtas numuru, sākot ar 1. Rindas numuram jābūt ievietotam pirmajās 3 pozīcijās, pielīdzinot labajai pusei, pēc kā jāseko vienam tukšumam, pēc kā seko rindas saturs. Drīkst pieņemt, ka ieejas failā ir mazāk nekā 1000 rindiņu. Piemēram,
 */
 
-//#include <fstream>
-//#include <iostream>
-//#include <string>
-//#include <iomanip>
-//using namespace std;
-//
-//int main()
-//{
-//   
-//    fstream fin("in.txt", ios::in);
-//    fstream fout("out.txt", ios::out);
-//    string s;
-//    getline(fin, s);
-//    int it = 1;
-//    while (fin)
-//    {
-//        fout << setw(3) << it <<' '<< s<<'\n';
-//        getline(fin, s);
-//        it++;
-//    };
-//    fin.close();
-//    fout.close();
-//    return 0;
-//}
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+   
+    fstream fin("in.txt", ios::in);
+    fstream fout("out.txt", ios::out);
+    string s;
+    getline(fin, s);
+    int it = 1;
+    while (fin)
+    {
+        fout << setw(3) << it <<' '<< s<<'\n';
+        getline(fin, s);
+        it++;
+    };
+    fin.close();
+    fout.close();
+    return 0;
+}
 
 
 
@@ -91,45 +91,45 @@ int main()
 
 /*B1[6]. Funkcija maxSpaceSequence, kas izskaita, cik gara ir garākā tukšumu virkne. Piemēram ('*' apzīmē tukšumu (' ')), “*alpha*beta***gamma*******delta” → 7*/
 
-//#include<iostream>
-//#include<fstream>
-//#include<string>
-//using namespace std;
-//
-//int countExtraAdjacentSpaces(fstream &fin) {
-//    
-//    string s;
-//    int gar = 0;
-//    while (fin) {
-//        getline(fin, s, '\n');
-//        int skaits = 0;
-//        for (int i = 0; i < s.length(); ++i) {
-//            if (s[i] == ' ') {
-//                skaits = 1;
-//                int k = ++i;
-//                while (s[k] == ' ' && k < s.length()) {
-//                    skaits++;
-//                    k++;
-//                }
-//                if (skaits > gar) {
-//                    gar = skaits;
-//                }
-//            }
-//        }
-//       
-//        return gar;
-//    }
-//}
-//int main(){
-//    fstream fin("in.txt", ios::in);
-//    
-//    if (fin.is_open())
-//    {
-//        cout<<countExtraAdjacentSpaces(fin);
-//        fin.close();
-//    }
-//}
-//
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int countExtraAdjacentSpaces(fstream &fin) {
+    
+    string s;
+    int gar = 0;
+    while (fin) {
+        getline(fin, s, '\n');
+        int skaits = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == ' ') {
+                skaits = 1;
+                int k = ++i;
+                while (s[k] == ' ' && k < s.length()) {
+                    skaits++;
+                    k++;
+                }
+                if (skaits > gar) {
+                    gar = skaits;
+                }
+            }
+        }
+       
+        return gar;
+    }
+}
+int main(){
+    fstream fin("in.txt", ios::in);
+    
+    if (fin.is_open())
+    {
+        cout<<countExtraAdjacentSpaces(fin);
+        fin.close();
+    }
+}
+
 
 
 /*B1[5]. Funkcija countExtraAdjacentSpaces, kas izskaita, cik failā ir lieku atkārtotu tukšumu
@@ -141,7 +141,7 @@ Piemēram (* apzīmē tukšumu), “alpha***beta**gamma” → 3
 #include<string>
 using namespace std;
 
-int countExtraAdjacentSpaces(fstream &fin) {
+int countExtraAdjacentSpaces(fstream& fin) {
     int cnt = 0;
     string s;
     while (getline(fin, s, '\n')) {
@@ -150,15 +150,15 @@ int countExtraAdjacentSpaces(fstream &fin) {
         }
     }
     return cnt;
-    
+
 
 }
 int main() {
     fstream fin("in.txt", ios::in);
-    
+
     if (fin.is_open())
     {
-        cout<<countExtraAdjacentSpaces(fin);
+        cout << countExtraAdjacentSpaces(fin);
         fin.close();
     }
 }
@@ -167,18 +167,18 @@ int main() {
 #include<fstream>
 using namespace std;
 int main() {
-    fstream fin ("in.txt",ios::in);
-    int cnt=0;
+    fstream fin("in.txt", ios::in);
+    int cnt = 0;
     string s;
-    if(fin.is_open())
+    if (fin.is_open())
     {
-        while(fin) {
-            getline(fin,s,'\n');
-            for (int i=1;i<s.length();++i) {
-                if (s[i-1]==' ' and s[i]==' ') ++cnt;
+        while (fin) {
+            getline(fin, s, '\n');
+            for (int i = 1; i < s.length(); ++i) {
+                if (s[i - 1] == ' ' and s[i] == ' ') ++cnt;
             }
         }
-        cout<<cnt<<endl;
+        cout << cnt << endl;
         fin.close();
     }
 }
@@ -194,7 +194,7 @@ Piemēram (* apzīmē tukšumu), “*alpha*beta***gamma*******delta” → “*a
 using namespace std;
 
 
-void removeExtraAdjacentSpaces(ifstream &fin, ofstream &fout) {
+void removeExtraAdjacentSpaces(ifstream& fin, ofstream& fout) {
     string s;
     while (getline(fin, s, '\n')) {
         string t;
@@ -220,7 +220,7 @@ int main() {
     if (fin.is_open())
     {
         removeExtraAdjacentSpaces(fin, fout);
-        
+
     }
 }
 
@@ -265,63 +265,61 @@ int main() {
 
 
 /*tikai ar int*/
-//int main() {
-//    int i;
-//    fstream fin("in.txt", ios::in);
-//    fin >> i;
-//    while (!fin.eof()) {  //faila objekta beigu pārbaude
-//        cout << i << endl;
-//        fin >> i;
-//    };
-//    fin.close();
-//    return 0;
-//}
+int main() {
+    int i;
+    fstream fin("in.txt", ios::in);
+    fin >> i;
+    while (!fin.eof()) {  //faila objekta beigu pārbaude
+        cout << i << endl;
+        fin >> i;
+    };
+    fin.close();
+    return 0;
+}
 
 
 /*saskaita, cik rindas beizas ar . , ;*/
-//#include<string>
-//
-//int main() {
-//	string s;
-//	fstream fin("in.txt", ios::in);
-//	getline(fin, s);
-//	int skaits = 0;
-//	while (fin)
-//	{
-//		if ((s[s.length() - 1]==';')|| (s[s.length() - 1] == ',')|| (s[s.length() - 1] == '.')) {
-//			skaits++;
-//		}
-//		cout << s << endl;
-//		getline(fin, s);
-//	};
-//	cout << skaits << endl;
-//	fin.close();
-//
-//}
+#include<string>
+
+int main() {
+	string s;
+	fstream fin("in.txt", ios::in);
+	getline(fin, s);
+	int skaits = 0;
+	while (fin)
+	{
+		if ((s[s.length() - 1]==';')|| (s[s.length() - 1] == ',')|| (s[s.length() - 1] == '.')) {
+			skaits++;
+		}
+		cout << s << endl;
+		getline(fin, s);
+	};
+	cout << skaits << endl;
+	fin.close();
+
+}
 /*Saskaitīt, cik ir burtu S*/
-//int main()
-//
-//{
-//
-//    fstream kk;
-//    char c;
-//    int filesize = 0;
-//    kk.open("lab19in.txt", ios::in);
-//    kk.get(c);
-//    while (kk) {
-//        if (c == 'S') filesize++;
-//        kk.get(c); //spēj nolasīt \n simbolu un saglabāt oriģinālo struktūru
-//
-//    };
-//
-//    kk.close();
-//
-//    cout << "S burtu skaits " << filesize << endl;
-//
-//    return 0;
-//
-//}
+int main()
 
+{
 
+    fstream kk;
+    char c;
+    int filesize = 0;
+    kk.open("lab19in.txt", ios::in);
+    kk.get(c);
+    while (kk) {
+        if (c == 'S') filesize++;
+        kk.get(c); //spēj nolasīt \n simbolu un saglabāt oriģinālo struktūru
+
+    };
+
+    kk.close();
+
+    cout << "S burtu skaits " << filesize << endl;
+
+    return 0;
+
+}
 
 
